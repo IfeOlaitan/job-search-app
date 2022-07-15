34b11cb01862b26ef6d4a6b7672ca23a3c7c7213
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-07-15 12:18:34 +0100

    new component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import { createApp } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faAngleDown,
-  faAngleUp,
-  faSearch,
-} from "@fortawesome/free-solid-svg-icons";
+import { faAngleDown, faAngleUp, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
