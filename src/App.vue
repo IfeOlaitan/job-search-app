e021a9ad8a7438e5705a9dfcf41142e3517915f1
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-07-15 12:40:41 +0100

    test component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <main-nav />
+    <job-create />
     <router-view />
   </div>
 </template>
 
 <script>
 import MainNav from "@/components/Navigation/MainNav.vue";
+import JobCreate from "@/components/JobResults/JobCreate";
 
 export default {
   name: "App",
   components: {
     MainNav,
+    JobCreate
   },
 };
 </script>
