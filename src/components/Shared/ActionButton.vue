ca2e012c16e24b91dfbb53498f34622f53dc1ada
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-07-20 12:54:00 +0100

    compositon

--- src/components/Shared/ActionButton.vue
+++ src/components/Shared/ActionButton.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { computed, toRefs } from "vue";
+
 export default {
   name: "ActionButton",
   props: {
@@ -21,13 +23,24 @@ export default {
       },
     },
   },
-  computed: {
-    buttonClass() {
+  setup(props) {
+    const { type } = toRefs(props);
+
+    const buttonClass = computed(() => {
       return {
-        [this.type]: true,
+        [type.value]: true,
       };
-    },
+    });
+
+    return { buttonClass };
   },
+  // computed: {
+  //   buttonClass() {
+  //     return {
+  //       [this.type]: true,
+  //     };
+  //   },
+  // },
 };
 </script>
 
--- src/playground.js
+++ src/playground.js
@@ -10,7 +10,8 @@
 //
 // console.log(sushi.slice(2, 5));
 
-//Sets
+//?Sets
+/*
 const numbers = new Set();
 
 numbers.add(5);
@@ -19,6 +20,49 @@ numbers.add(5);
 numbers.add(15);
 
 console.log(numbers);
+*/
+
+const { ref, computed, reactive, toRefs }  = require("vue");
+
+//?Reactivity
+//Number
+/*
+let a = ref(1);
+let b = ref(2);
+let c = computed(() => a.value + b.value);
+console.log(c.value);
+
+a.value = 10;
+console.log(c.value);
+ */
+
+//String
+/*
+const name = ref("ife");
+const job = ref("ux dev");
+const title = computed(() => name.value + ' ' + job.value);
+console.log(title.value);
+
+name.value = 'olaitan';
+console.log(title.value);
+ */
+
+//Object
+const person = reactive({
+  firstName: 'Ifeoluwa',
+  lastName: 'Olaitan'
+});
+// const {firstName, lastName} = toRefs(person);
+// const title = computed(() => `${firstName.value} ${lastName.value} the Great!`);
+//
+// person.firstName = 'Ayo';
+// console.log(title.value);
+//
+// person.firstName = 'Dele';
+// console.log(title.value);
+
+const refPerson = toRefs(person);
+console.log(person.firstName);
 
 
 
